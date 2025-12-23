4328666191e63ce85096f27d8ef3f949ac400edb
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2025-12-23 13:03:58 -0500

    Initial commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+// import the components here as such 
+import HomePage from './components/homepage.jsx'; 
+
+function App() {
+
+  return (
+    <>
+      <HomePage/>
+    </>
+  )
+}
+
+export default App
---
+++ my-react-app/src/components/homepage.jsx
@@ -0,0 +1,5 @@
+export default function HomePage() { 
+    return( 
+        <h1>Working on new Portfolio website for myself</h1>
+    );
+}
